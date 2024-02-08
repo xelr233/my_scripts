0823e710080bc49ae511ea0ebc2bce27c56c8949
xelr233 <yang2337647395@gmail.com>
2024-02-09 05:49:38 +0800

    2月9日-3

--- check_AkileCloud.py
+++ check_AkileCloud.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding=utf-8
 """
 Author: Xe
@@ -15,11 +15,11 @@ import os
 #title = "监控_AkileCloud"
 
 config = {
-    'PUSH_PLUS_TOKEN':'',
-    'TITLE':''
+    'PUSH_PLUS_TOKEN':'62871993f33d4750b4a6e6ddafc2d035',
+    'TITLE':'监控_AkileCloud'
 }
 ###
-for k in config:
+""" for k in config:
     v = os.getenv(k)
     if v is not None:
         if v == '':
@@ -29,7 +29,7 @@ for k in config:
             print(f"环境变量 {k} 的值为: {v}")
     else:
         print(f"缺少环境变量 {k}")
-        exit()
+        exit() """
 url = "https://api.akile.io/api/v1/store/GetVpsStore"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
 counter = 0
@@ -87,7 +87,7 @@ body = {
     "template":"html"
 }
 response = requests.post(url = send_url,headers=headers,params=body)
-if response.ok == True:
+if json.loads(response.text)['code'] == 200:
     print("发送成功")
 else:
     print("发送失败")
